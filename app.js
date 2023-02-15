3f019b6c058a7fce117987f0b73781515a85650f
Marco <marco.mayorga@gmail.com>
2023-02-14 22:24:10 -0600

    Refactor the whole script and fix functionality

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// Constants
-const addBookButton = document.getElementById("add-book-button");
-
-
-// Prevents default action and doesn't submit form on click.
-const form = document.getElementById("form");
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
-
+// List of Books
 let myLibrary = [];
+
+// Book class
 class Book {
     constructor(author, title, pages, read) {
         this.author = author;
@@ -19,75 +12,92 @@ class Book {
 }
 
 
-// Adds books to myLibrary list
-function addBookToLibrary() {
-    // Grabbing the input values
-    const author = document.getElementById("author").value;
-    const title = document.getElementById("title").value;
-    const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").checked;
+const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+    // This stops form from submitting
+    event.preventDefault();
 
-    const newBook = new Book(author, title, pages, read);
-    // Making sure that the values aren't empty
-    if (author.length != 0 || title.length != 0 || pages.length != 0) {
+    // Grabs the input values
+    const author = document.getElementById("author");
+    const title = document.getElementById("title");
+    const pages = document.getElementById("pages");
+    const read = document.getElementById("read");
+
+    // Gets the table element
+    const table = document.getElementById("book-table");
+
+    // Inserts new row into table and adds a class of row
+    const newBookRow = table.insertRow();
+    newBookRow.classList.add("books");
+
+    // Puts cells into rows
+    const authorCell = newBookRow.insertCell(0);
+    const titleCell = newBookRow.insertCell(1);
+    const pagesCell = newBookRow.insertCell(2);
+    const readCell = newBookRow.insertCell(3);
+    const deleteCell = newBookRow.insertCell(4);
+
+    // Creates A new class instance
+    const newBook = new Book(author.value, title.value, pages.value, read.checked);
+    // Making sure that the values aren't empty and then creates the table row and eachc cell
+    if (author.value.length != 0 || title.value.length != 0 || pages.value.length != 0) {
+        // Moves the data to the list
         myLibrary.push(newBook);
-        console.log(myLibrary);
-        displayBook();
-    }
 
-}
+        // Making the inner html of each cell the value of each input box
+        authorCell.innerHTML = author.value;
+        titleCell.innerHTML = title.value;
+        pagesCell.innerHTML = pages.value;
 
-function deleteBookFromList(index) {
-    myLibrary.splice(index, 1);
-}
-
-// Loops through books in list and displays them
-let lastIndex = 0
-function displayBook() {
-    // Loops through all items in list
-    for (let i = lastIndex; i < myLibrary.length; i++) {
-
-        let table = document.getElementById("book-table");
-        // inserting first row with book data and inserting the cells into the row
-        let newBookRow = table.insertRow(i + 1);
-        let authorCell = newBookRow.insertCell(0);
-        let titleCell = newBookRow.insertCell(1);
-        let pagesCell = newBookRow.insertCell(2);
-        let readCell = newBookRow.insertCell(3);
-        let deleteCell = newBookRow.insertCell(4);
-        // Making the inner html of each cell what it needs to be
-        authorCell.innerHTML = myLibrary[i].author;
-        titleCell.innerHTML = myLibrary[i].title;
-        pagesCell.innerHTML = myLibrary[i].pages;
-        // Checks if read is true or not and creates the checkbox element
-        let readCheckbox = document.createElement("input");
+        // Creates the checkbox element for each item
+        const readCheckbox = document.createElement("input");
         readCheckbox.setAttribute("type", "checkbox");
-        readCell.appendChild(readCheckbox);
-        if (myLibrary[i].read === true) {
+        readCheckbox.classList.add("checkbox");
+        readCheckbox.classList.add("checkbox-in-list");
+
+        // Checks if read is true or not so it adds it correctly to the new book node
+        if (read.checked === true) {
             readCheckbox.checked = true;
         } else {
             readCheckbox.checked = false;
         }
 
-        // Delete button
-        let deleteButton = document.createElement("button");
-        let buttonText = document.createTextNode("Delete");
-        deleteButton.classList = "delete-button"
-        deleteButton.appendChild(buttonText);
-        deleteCell.appendChild(deleteButton);
-        // Updates the last object (book) index
-        deleteButton.addEventListener("click", deleteBookFromList(i));
+        // Makes delete button for each item
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("button");
+        deleteButton.classList.add("delete");
+        deleteButton.innerHTML = "Delete";
 
-        lastIndex = i + 1;
-        console.log(lastIndex)
+        // Appends child to the cell it belongs in
+        readCell.appendChild(readCheckbox);
+        deleteCell.appendChild(deleteButton);
+
+        // Clears the input for the next book
+        author.value = "";
+        title.value = "";
+        pages.value = "";
+        read.checked = false;
+        console.log(myLibrary);
+
+        // Checks the read checkbox and updates the list
+        // I subtract one from index because of the title Row
+        readCheckbox.addEventListener("click", () => {
+            if (readCheckbox.checked === true) {
+                myLibrary[newBookRow.rowIndex - 1]["read"] = true;
+            } else if (readCheckbox.checked === false) {
+                myLibrary[newBookRow.rowIndex - 1]["read"] = false;
+            }
+        });
+
+        // Deletes row noded from document and updates the list
+        // I subtract one from index because of the title Row
+        deleteButton.addEventListener("click", () => {
+            const tableHeader = document.getElementById("tbody-of-books")
+            tableHeader.removeChild(newBookRow);
+            myLibrary.splice(myLibrary[newBookRow.rowIndex - 1], 1)
+            console.log(myLibrary);
+        })
     }
 
 
-}
-
-
-// Calls the addBooksToLibrary Function when addbookbutton is pressed.
-addBookButton.addEventListener("click", addBookToLibrary);
-
-// TO_DO
-// Make A button to remove one book
+});
